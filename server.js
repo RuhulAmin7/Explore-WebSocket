136bcd612d9e55f98f51173454a9c859eb3a56cf
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2022-09-21 09:13:22 +0600

    listening port chage

--- server.js
+++ server.js
@@ -1,35 +1,30 @@
 const express = require('express');
 const http = require('http');
 const app = express();
-const expressHTTPServer = http.createServer(app)
+const expressHTTPServer = http.createServer(app);
 const ws = require('ws');
-const wss = new ws.Server({server: expressHTTPServer});
- 
+const wss = new ws.Server({ server: expressHTTPServer });
 
-app.get('/', (req, res)=>{
-    res.sendFile(`${__dirname}/index.html`)
-})
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
 
-wss.on('connection', (socket)=>{
-    console.log('User connected')
-    // receive user message
-    socket.on('message', (data)=>{
-        const msg = data.toString();
-        wss.clients.forEach(client =>{
-            if(client !== socket){
-                client.send(msg);
-            }
-        })
-
+wss.on('connection', (socket) => {
+  console.log('User connected');
+  // receive user message
+  socket.on('message', (data) => {
+    const msg = data.toString();
+    wss.clients.forEach((client) => {
+      if (client !== socket) {
+        client.send(msg);
+      }
     });
+  });
 
-    // user close connection
-    socket.on('close', (data)=>{
-        console.log('User Disconnected');
-    });
-})
+  // user close connection
+  socket.on('close', (data) => {
+    console.log('User Disconnected');
+  });
+});
 
-
-
-
-expressHTTPServer.listen('8080', ()=> console.log('Server is running at http://localhost:8080'))  
+expressHTTPServer.listen();
